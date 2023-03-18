ebf3912130dc38534420857ddeb62d4849e93112
José Rafael <rafaelmatiashue@gmail.com>
2023-03-18 20:57:19 -0300

    començando o app

---
+++ List/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from List.models import *
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Supermarket)
+admin.site.register(Product)
+admin.site.register(HistoricalPrice)
